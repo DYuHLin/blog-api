d56b52e650efec6ce4250784a5f503037d38c9e7
DYuHLin <damianver2000@gmail.com>
2024-03-20 16:20:13 +0200

    Currently working on routes and testing with postman. So far everything is fine.

--- server/server.js
+++ server/server.js
@@ -5,4 +5,32 @@ const app = express();
 
 app.use(cors());
 
+app.get("/posts", (req, res, next) => {
+    return res.send("GET Method for posts");
+});
+
+app.get("/posts/:id", (req, res, next) => {
+    return res.send("GET Method for specific post");
+});
+
+app.get("/posts/create", (req, res, next) => {
+    return res.send("GET Method for create post");
+});
+
+app.get("/posts/:id/update", (req, res, next) => {
+    return res.send("GET Method for update post");
+});
+
+app.get("/posts/:id/delete", (req, res, next) => {
+    return res.send("GET Method for delete post");
+});
+
+app.get("/posts/login", (req, res, next) => {
+    return res.send("GET Method for login");
+});
+
+app.get("/posts/register", (req, res, next) => {
+    return res.send("GET Method for register");
+});
+
 app.listen(5000, () => console.log("Listening on port 5000"));
