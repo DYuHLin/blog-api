16bc6a95583e9a52bcd1fe8ae40ccf0920179149
DYuHLin <damianver2000@gmail.com>
2024-04-03 15:26:07 +0200

    I have completed my update and delete requests for my posts and now I am going to implement it on the frontend.

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -2,6 +2,10 @@ const asyncHandler = require('express-async-handler');
 const {body, validationResult} = require('express-validator');
 const posts = require('../models/posts');
 
+exports.get_posts = asyncHandler(async (req, res, next) => {
+    await posts.find().populate("user").exec();
+});
+
 exports.post_post = asyncHandler(async (req, res, next) => {
 
     const errors = validationResult(req);
@@ -19,6 +23,31 @@ exports.post_post = asyncHandler(async (req, res, next) => {
     } else {
         await post.save();
     };
+});
 
-    const newPost = post.save();
+exports.update_post = asyncHandler(async (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    const post = new posts({
+        user: req.body.user,
+        title: req.body.title,
+        content: req.body.content,
+        date: Date.now(),
+        published: req.body.published,
+        _id: req.params.id
+
+        });
+
+    if(!errors.isEmpty()){
+        return console.log(errors);
+    } else {
+        await posts.findByIdAndUpdate(req.params.id, post, {});
+    };
+});
+
+exports.post_delete_post = asyncHandler(async (req, res, next) => {
+    const post = await posts.findById(req.params.id).populate("user").exec();
+
+    await posts.findByIdAndDelete(req.body.postid);
 });
