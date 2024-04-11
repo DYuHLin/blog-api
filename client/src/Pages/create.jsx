b776125fe7fbd3be54c2af8c0944f0138f8b05c3
DYuHLin <damianver2000@gmail.com>
2024-04-11 15:58:12 +0200

    I got the tinyMCE to work using their onInit function and the useRef hook.

--- client/src/Pages/create.jsx
+++ client/src/Pages/create.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, useRef} from 'react'
 import { Editor } from '@tinymce/tinymce-react'
 import {jwtDecode} from 'jwt-decode'
 import UserContext from '../UserContext';
@@ -10,17 +10,20 @@ function create() {
 
   const {user} = useContext(UserContext);
 
+  const ContentRef = useRef();
+
   const showToken = (e) => {
     e.preventDefault();
-    const decoded = jwtDecode(user.accessToken)
-    console.log(decoded.user._id)
-    console.log(published)
+    // const decoded = jwtDecode(user.accessToken)
+    // console.log(decoded.user._id)
+    console.log(ContentRef.current.getContent());
+   
   }
 
   const handleSubmit = (e) => { 
     e.preventDefault();
     const decoded = jwtDecode(user.accessToken);
-    const post = {user: decoded.user._id, title: title, content: content, published: published};
+    const post = {user: decoded.user._id, title: title, content: ContentRef.current.getContent(), published: published};
   
     fetch('http://localhost:5000/api/create', {
       method: "POST",
@@ -37,8 +40,9 @@ function create() {
       <label htmlFor="title">Title: </label>
         <input type="text" required name='title' id='title' className='title' onChange={(e) => setTitle(e.target.value)}/>
         <label htmlFor="body">Body: </label>
-        {/* <Editor apiKey='7b9bztrodn0kidftvkbg5tuk6lqiwpwtl934lt1s1av1ghzr' name="body" id="body" required className='body' onChange={(e) => setContent(e.target.value)}/> */}
-        <textarea name="body" id="body" className='body' cols="30" rows="10" onChange={(e) => setContent(e.target.value)}></textarea>
+        <Editor apiKey='7b9bztrodn0kidftvkbg5tuk6lqiwpwtl934lt1s1av1ghzr' name="body" id="body" required className='body' 
+          onInit={(evt, editor) => ContentRef.current = editor}/>
+        {/* <textarea name="body" id="body" className='body' cols="30" rows="10" onChange={(e) => setContent(e.target.value)}></textarea> */}
         <label htmlFor="publish">Publish: </label>
         <input type="checkbox" id='publish' name='publish' className='publish' onChange={(e) => setPublished(e.target.value)}/>
         <button>Post Blog</button>
